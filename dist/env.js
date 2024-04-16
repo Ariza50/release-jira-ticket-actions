54ca9fe4de704b5cdb3958410754b8fddb6419cd
Vicente Albert <capi.valbert@iberia.es>
2024-04-16 19:24:38 +0200

    build project

--- dist/env.js
+++ dist/env.js
@@ -23,17 +23,16 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DRY_RUN = exports.TICKETS = exports.CREATE = exports.PROJECT = exports.RELEASE_NAME = exports.SUBDOMAIN = exports.API_TOKEN = exports.EMAIL = void 0;
+exports.DRY_RUN = exports.GH_REPOSITORY = exports.GH_TOKEN = exports.GH_USER = exports.PROJECT = exports.RELEASE_NAME = exports.SUBDOMAIN = exports.API_TOKEN = exports.EMAIL = void 0;
 const core = __importStar(require("@actions/core"));
 exports.EMAIL = core.getInput('email', { required: true });
 exports.API_TOKEN = core.getInput('api_token', { required: true });
 exports.SUBDOMAIN = core.getInput('subdomain', { required: true });
 exports.RELEASE_NAME = core.getInput('release_name', {
-    required: true
-});
-exports.PROJECT = core.getInput('jira_project', { required: true });
-exports.CREATE = core.getInput('create', {
     required: false
 });
-exports.TICKETS = core.getInput('tickets', { required: false });
+exports.PROJECT = core.getInput('jira_project', { required: true });
+exports.GH_USER = core.getInput('gh_user', { required: true });
+exports.GH_TOKEN = core.getInput('gh_token', { required: true });
+exports.GH_REPOSITORY = core.getInput('gh_repository', { required: true });
 exports.DRY_RUN = core.getInput('dry_run', { required: false });
